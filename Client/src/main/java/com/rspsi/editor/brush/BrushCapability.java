fca8459433244c7292b11a86312a371b35b807a6
Neosback <sythril2010@gmail.com>
2026-09-20 07:46:02 -0500

    Add neutral brush contracts and spatial brushes

---
+++ Client/src/main/java/com/rspsi/editor/brush/BrushCapability.java
@@ -0,0 +1,10 @@
+package com.rspsi.editor.brush;
+
+/** Capabilities advertised by neutral editor brush contributions. */
+public enum BrushCapability {
+    SPATIAL_FOOTPRINT,
+    WEIGHTED_FALLOFF,
+    HEIGHT_MANIPULATION,
+    TILE_PAINT,
+    OBJECT_SCATTER
+}
