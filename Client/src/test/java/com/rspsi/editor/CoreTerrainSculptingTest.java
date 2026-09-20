8891265ac28a00101662e552240eb03cfd65cc1c
Neosback <sythril2010@gmail.com>
2026-09-20 11:13:34 -0500

    Finish typed viewport migration in tests

--- Client/src/test/java/com/rspsi/editor/CoreTerrainSculptingTest.java
+++ Client/src/test/java/com/rspsi/editor/CoreTerrainSculptingTest.java
@@ -99,7 +99,7 @@ class CoreTerrainSculptingTest {
 
     private static ToolContext context(EditorSession session) {
         return new ToolContext(session, new EmptyAssets(),
-                (x, y) -> Optional.of(new TileCoordinate(0, (int) x, (int) y)));
+                (x, y) -> Optional.of(new WorldTile(0, (int) x, (int) y)));
     }
 
     private static PointerEvent pointer(float x, float y) {
