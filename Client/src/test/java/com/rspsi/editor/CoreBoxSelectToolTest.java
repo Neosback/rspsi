e412112c5ee8da1faf5cf4cefea926c57d5d3d00
Neosback <sythril2010@gmail.com>
2026-09-20 11:13:16 -0500

    Finish typed viewport migration in tests

--- Client/src/test/java/com/rspsi/editor/CoreBoxSelectToolTest.java
+++ Client/src/test/java/com/rspsi/editor/CoreBoxSelectToolTest.java
@@ -164,7 +164,7 @@ class CoreBoxSelectToolTest {
 
     private static ToolContext context(EditorSession session) {
         return new ToolContext(session, new EmptyAssets(),
-                (x, y) -> Optional.of(new TileCoordinate(0, (int) x, (int) y)));
+                (x, y) -> Optional.of(new WorldTile(0, (int) x, (int) y)));
     }
 
     private static PointerEvent pointer(float x, float y) {
