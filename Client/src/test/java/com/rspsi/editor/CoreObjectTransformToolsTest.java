1bb70ab0db9fc48ccdc25d38d64614dffef28e44
Neosback <sythril2010@gmail.com>
2026-09-20 11:11:21 -0500

    Update editor tests for typed world viewport coordinates

--- Client/src/test/java/com/rspsi/editor/CoreObjectTransformToolsTest.java
+++ Client/src/test/java/com/rspsi/editor/CoreObjectTransformToolsTest.java
@@ -6,6 +6,7 @@ import com.rspsi.editor.model.TileCoordinate;
 import com.rspsi.editor.model.TileSnapshot;
 import com.rspsi.editor.model.WorldDocument;
 import com.rspsi.editor.model.WorldObject;
+import com.rspsi.editor.model.WorldTile;
 import com.rspsi.editor.tool.DuplicateObjectTool;
 import com.rspsi.editor.tool.DuplicateSelectionTool;
 import com.rspsi.editor.tool.EditorToolController;
@@ -110,7 +111,7 @@ class CoreObjectTransformToolsTest {
         put(world, object);
         EditorSession session = new EditorSession(world);
         Viewport viewport = new Viewport() {
-            @Override public Optional<TileCoordinate> tileAt(float x, float y) {
+            @Override public Optional<WorldTile> tileAt(float x, float y) {
                 return Optional.of(new TileCoordinate(0, 1, 1));
             }
 
