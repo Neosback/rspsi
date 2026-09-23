53cb1460e812fee23c7e4c7e884df4848ebdbffb
Neosback <sythril2010@gmail.com>
2026-09-23 09:48:42 -0500

    Let cache-scoped definition workspace outlive close callbacks

--- Client/src/main/java/com/rspsi/cache/workspace/LoadedOsrsCacheSession.java
+++ Client/src/main/java/com/rspsi/cache/workspace/LoadedOsrsCacheSession.java
@@ -63,7 +63,6 @@ public final class LoadedOsrsCacheSession implements AutoCloseable {
 
     @Override
     public void close() {
-        objectDefinitions.clear();
         bundle.close();
     }
 }
