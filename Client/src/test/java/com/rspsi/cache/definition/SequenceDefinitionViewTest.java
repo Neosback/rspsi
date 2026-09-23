512454f413a16b8d1af610c0b73cbfb8df664dea
Neosback <sythril2010@gmail.com>
2026-09-23 03:59:15 -0500

    Test cached skeletal sequence range contract

--- Client/src/test/java/com/rspsi/cache/definition/SequenceDefinitionViewTest.java
+++ Client/src/test/java/com/rspsi/cache/definition/SequenceDefinitionViewTest.java
@@ -25,6 +25,21 @@ class SequenceDefinitionViewTest {
     }
 
     @Test
+    void retainsCachedSkeletalFrameRange() {
+        SequenceDefinitionView sequence = new SequenceDefinitionView(
+                42, new int[0], new int[0], 3, false,
+                -1, -1, 99, 0, 0, 2,
+                0x12340005, 10, 18, -7);
+
+        assertEquals(0x12340005, sequence.skeletalId());
+        assertEquals(10, sequence.skeletalRangeBegin());
+        assertEquals(18, sequence.skeletalRangeEnd());
+        assertEquals(8, sequence.cachedFrameCount());
+        org.junit.jupiter.api.Assertions.assertTrue(sequence.cachedSkeletal());
+        assertEquals(-7, sequence.animationHeightOffset());
+    }
+
+    @Test
     void rejectsOffsetsOutsideTheSignedClientByteRange() {
         assertThrows(IllegalArgumentException.class, () -> new SequenceDefinitionView(
                 42, new int[]{100}, new int[]{3}, 1, false,
