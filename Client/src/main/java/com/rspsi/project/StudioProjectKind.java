0597fb4a777b206ddefb703cec26bfd63ef8b586
Neosback <sythril2010@gmail.com>
2026-09-24 10:28:34 -0500

    Add persistent Studio project kinds

---
+++ Client/src/main/java/com/rspsi/project/StudioProjectKind.java
@@ -0,0 +1,7 @@
+package com.rspsi.project;
+
+/** Top-level project kinds understood by the application launcher. */
+public enum StudioProjectKind {
+    STANDALONE_OSRS_CACHE,
+    OPENRUNE_SERVER
+}
