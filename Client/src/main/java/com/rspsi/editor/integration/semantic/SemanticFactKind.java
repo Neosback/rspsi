5bf579ebb87dbe1f570b2e778c54b4d4a750c579
Neosback <sythril2010@gmail.com>
2026-09-24 08:21:23 -0500

    Add semantic source fact kinds

---
+++ Client/src/main/java/com/rspsi/editor/integration/semantic/SemanticFactKind.java
@@ -0,0 +1,14 @@
+package com.rspsi.editor.integration.semantic;
+
+/** Stable neutral categories produced by server/source semantic adapters. */
+public enum SemanticFactKind {
+    CLASS_DECLARATION,
+    FUNCTION_DECLARATION,
+    CALL,
+    SYMBOL_REFERENCE,
+    PLUGIN_SCRIPT,
+    QUEST_SCRIPT,
+    SCRIPT_HANDLER,
+    QUEST_DEFINITION,
+    VAR_BINDING
+}
