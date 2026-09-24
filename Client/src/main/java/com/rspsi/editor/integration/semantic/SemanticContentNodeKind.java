86230bdc44256f9a10712aa0d2ee51ca465f1c86
Neosback <sythril2010@gmail.com>
2026-09-24 09:14:12 -0500

    Add semantic content graph node kinds

---
+++ Client/src/main/java/com/rspsi/editor/integration/semantic/SemanticContentNodeKind.java
@@ -0,0 +1,10 @@
+package com.rspsi.editor.integration.semantic;
+
+/** Stable high-level node categories in the Studio content graph. */
+public enum SemanticContentNodeKind {
+    SYMBOL,
+    SCRIPT,
+    QUEST,
+    HANDLER,
+    RESOURCE
+}
