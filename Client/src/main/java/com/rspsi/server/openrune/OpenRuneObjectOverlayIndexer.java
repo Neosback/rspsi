46a4636b4cc0596363c1e96aeaa048d93136db85
Neosback <sythril2010@gmail.com>
2026-09-25 03:56:36 -0500

    fix: align OpenRune integration package declarations

--- Client/src/main/java/com/rspsi/server/openrune/OpenRuneObjectOverlayIndexer.java
+++ Client/src/main/java/com/rspsi/server/openrune/OpenRuneObjectOverlayIndexer.java
@@ -1,4 +1,4 @@
-package com.rspsi.plugins.server.openrune;
+package com.rspsi.server.openrune;
 
 import com.rspsi.editor.integration.semantic.ServerObjectSemanticIndex;
 import com.rspsi.editor.integration.semantic.ServerObjectSemanticOverlay;
