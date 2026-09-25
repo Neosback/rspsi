f4654b4825a299f0b90f67de27ddc60187421038
Neosback <sythril2010@gmail.com>
2026-09-25 03:57:01 -0500

    fix: align OpenRune integration package declarations

--- Client/src/test/java/com/rspsi/server/openrune/OpenRuneServerProviderTest.java
+++ Client/src/test/java/com/rspsi/server/openrune/OpenRuneServerProviderTest.java
@@ -1,4 +1,4 @@
-package com.rspsi.plugins.server.openrune;
+package com.rspsi.server.openrune;
 
 import com.rspsi.editor.integration.IntegrationCapability;
 import com.rspsi.editor.integration.IntegrationOptions;
