76a000e8e1d7807897ca66955529db1cb32dafbd
Neosback <sythril2010@gmail.com>
2026-09-25 03:56:32 -0500

    fix: align OpenRune integration package declarations

--- Client/src/main/java/com/rspsi/server/openrune/OpenRuneNpcSpawnProvider.java
+++ Client/src/main/java/com/rspsi/server/openrune/OpenRuneNpcSpawnProvider.java
@@ -1,4 +1,4 @@
-package com.rspsi.plugins.server.openrune;
+package com.rspsi.server.openrune;
 
 import com.rspsi.editor.integration.content.ParseDiagnostics;
 import com.rspsi.editor.integration.npc.NpcSpawn;
