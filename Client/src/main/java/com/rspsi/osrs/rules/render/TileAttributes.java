f2b353c7867c6364cce40d6dab6fdd1200454311
Neosback <sythril2010@gmail.com>
2026-09-26 08:43:44 -0500

    Rehome used org.major constants under rspsi packages

---
+++ Client/src/main/java/com/rspsi/osrs/rules/render/TileAttributes.java
@@ -0,0 +1,10 @@
+package com.rspsi.osrs.rules.render;
+
+/** Scene traversal attribute masks for tile-neighbour rendering. */
+public class TileAttributes {
+
+    public static int RENDER_TILE_NORTH = 8;
+    public static int RENDER_TILE_SOUTH = 2;
+    public static int RENDER_TILE_WEST = 1;
+    public static int RENDER_TILE_EAST = 4;
+}
